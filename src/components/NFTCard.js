3d9aa14fc1163bd1c9dae633d4debca3f0a6362b
You Song Hou <yousonghou@gmail.com>
2022-01-15 22:46:57 -0500

    refactor: improve Card Image UI

--- src/components/NFTCard.js
+++ src/components/NFTCard.js
@@ -15,14 +15,14 @@ function NFTCard({ nftdata }) {
     <Col xs={24} sm={12} md={8} lg={6}>
       <Card
         hoverable
-        cover={<Image alt="NFT Image" src={nftdata.external_data.image ? nftdata.external_data.image : getURLImage(nftdata.token_url)} />}
+        cover={<Image className="card-image" alt="NFT Image" src={nftdata.external_data.image ? nftdata.external_data.image : getURLImage(nftdata.token_url)} />}
       >
         <Tabs defaultActiveKey="1">
           <Tabs.TabPane tab="Contract Info" key="1">
             <h4>{nftdata.contract_address}</h4>
             <p>Id: {nftdata.token_id}</p>
           </Tabs.TabPane>
-          
+
           <Tabs.TabPane tab="NFT Data" key="2">
             {nftdata.external_data.image
               ? <>
