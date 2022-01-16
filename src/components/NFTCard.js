622e4da54e6c5863b1a78d69aec7b0c712288631
You Song Hou <yousonghou@gmail.com>
2022-01-15 22:40:30 -0500

    feat: add a tab in Ant Design Card

--- src/components/NFTCard.js
+++ src/components/NFTCard.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Col, Card, Image } from 'antd';
+import React, { useState } from 'react';
+import { Col, Card, Tabs, Image } from 'antd';
 
 function NFTCard({ nftdata }) {
   const getURLImage = url => {
@@ -10,33 +10,32 @@ function NFTCard({ nftdata }) {
       return `https://ipfs.io/ipfs/${url}`;
     }
   }
-  
-  if(nftdata.external_data.image){
-    return (
-      <Col xs={24} sm={12} md={8} lg={6}>
-        <Card
-          hoverable
-          cover={<Image alt="NFT Image" src={nftdata.external_data.image} />}
-        >
-          <Card.Meta title={nftdata.external_data.name} description={nftdata.external_data.description} />
-          <p>{nftdata.contract_address}</p>
-        </Card>
-      </Col>
-    )
-  }
-  else{
-    return (
-      <Col xs={24} sm={12} md={8} lg={6}>
-        <Card
-          hoverable
-          cover={<Image alt="NFT Image" src={getURLImage(nftdata.token_url)} />}
-        >
-          <Card.Meta title={nftdata.token_id}/>
-          <p>{nftdata.contract_address}</p>
-        </Card>
-      </Col>
-    )
-  }
+
+  return (
+    <Col xs={24} sm={12} md={8} lg={6}>
+      <Card
+        hoverable
+        cover={<Image alt="NFT Image" src={nftdata.external_data.image ? nftdata.external_data.image : getURLImage(nftdata.token_url)} />}
+      >
+        <Tabs defaultActiveKey="1">
+          <Tabs.TabPane tab="Contract Info" key="1">
+            <h4>{nftdata.contract_address}</h4>
+            <p>Id: {nftdata.token_id}</p>
+          </Tabs.TabPane>
+          
+          <Tabs.TabPane tab="NFT Data" key="2">
+            {nftdata.external_data.image
+              ? <>
+                  <Card.Meta title={nftdata.external_data.name} description={nftdata.external_data.description} />
+                  <p>{nftdata.contract_address}</p>
+                </>
+              : <p>None</p>
+            }
+          </Tabs.TabPane>
+        </Tabs>
+      </Card>
+    </Col>
+  )
 }
 
 export default NFTCard;
